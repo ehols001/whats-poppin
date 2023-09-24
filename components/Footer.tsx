c0565287008bba6481ef9e4033136cf976c37306
eholster <ehols001@odu.edu>
2023-09-24 18:45:18 -0400

    added initial footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='flex justify-between p-8 bg-slate-100'>
+        Footer
+    </section>
+  )
+}
+
+export default Footer;
