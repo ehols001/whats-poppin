77274930a2eed4c33fd827343578a052351a2a25
eholster <ehols001@odu.edu>
2023-09-25 16:38:49 -0400

    got all service related pages routing properly

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,56 +1,53 @@
 'use client'
 
-import Link from 'next/link'
-import Image from 'next/image'
-import { platforms, genres } from '@/lib'
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
+
+import { services, genres } from '@/lib/demos';
 import { signIn, signOut } from 'next-auth/react';
+import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Header({ user }: { user: any }) {
     const [service, setService] = React.useState('');
     const index = Number(service);
-    const [genre, setGenre] = React.useState('Pop');
-    const [numSongs, setNumSongs] = React.useState('10');
+    //const [genre, setGenre] = React.useState('Pop');
+    //const [numSongs, setNumSongs] = React.useState('10');
 
     return (
         <div className='flex flex-col p-8 bg-slate-100'>
             <div className='flex justify-between'>
-                <Link href={platforms[index].link}>
+                <Link href={services[index].link}>
                     <Image
-                        src={platforms[index].logo}
+                        src={services[index].logo}
                         width={160}
                         height={160}
-                        alt={platforms[index].title}
+                        alt={services[index].title}
                     />
                 </Link>
-                {user ? (
-                    <button onClick={() => signOut()}>Sign Out</button>
-                ) : (
-                    <button onClick={() => signIn()}>Sign In</button>
-                )}
+                <div className='flex items-center justify-between'>
+                    {user ? (
+                        <button onClick={() => signOut()} className='flex items-center justify-center bg-black text-white h-9 mx-4 px-3 rounded-xl'>Sign Out</button>
+                    ) : (
+                        <button onClick={() => signIn()} className='flex items-center justify-center bg-black text-white h-9 mx-4 px-3 rounded-xl'>Sign In</button>
+                    )}
+
+                    <Link href='/services/service-settings'>
+                        <div className='w-9 h-9 flex items-center justify-center bg-slate-400 rounded-xl'>
+                            <FontAwesomeIcon icon={faGear} size='xl' />
+                        </div>
+                    </Link>
+                </div>
             </div>
 
-            <h1>Top {numSongs} {genre} Songs</h1>
+            {/* <h1>Top {numSongs} {genre} Songs</h1> */}
 
             <div className='flex justify-between mt-8'>
-                <select className='mr-4' onChange={(event) => setService(event.currentTarget.value)}>
-                    {platforms.map((platform, index) => (
-                        <option key={platform.title} value={index}>{platform.title}</option>
+                <div className='mr-4'>
+                    {services.map((service, index) => (
+                        <Link href={`/services/${service.serviceId}`}>{service.title}{' '}</Link>
                     ))}
-                </select>
-                <div>
-                    <select className='mr-4' onChange={(event) => setGenre(event.currentTarget.value)}>
-                        {genres.map((genre) => (
-                            <option key={genre.id} value={genre.title}>{genre.title}</option>
-                        ))}
-                    </select>
-                    <select onChange={(event) => setNumSongs(event.currentTarget.value)}>
-                        <option value="10">10</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                        <option value="200">200</option>
-                        <option value="500">500</option>
-                    </select>
                 </div>
             </div>
         </div>
