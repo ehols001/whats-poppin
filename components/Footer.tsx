4a2e9c7b61be4346972f6fdd5c4632b4ace5a167
eholster <ehols001@odu.edu>
2023-09-29 14:34:45 -0400

    adjusted some margins

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,26 +3,24 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <div className='flex items-center mt-8'>
-            <div className='flex flex-col text-center text-sm'>
-                <p>Designed and developed by 
-                    <Link
-                        href="https://www.linkedin.com/in/evan-holster-b1b909239/"
-                        target='_blank'
-                    >
-                        <span className='font-bold'> Evan Holster</span>
-                    </Link>
-                </p>    
-                <p>using Next.js, React, Node.js, TypeScript, Prisma, and Tailwind</p>
-                <p className='mt-2'>Deployed on 
-                    <Link 
-                        href="https://www.vercel.com"
-                        target='_blank'
-                    >
-                        <span className='font-bold'> Vercel</span>
-                    </Link>
-                </p>
-            </div>
+        <div className='flex flex-col justify-center text-center text-sm my-8'>
+            <p>Designed and developed by
+                <Link
+                    href="https://www.linkedin.com/in/evan-holster-b1b909239/"
+                    target='_blank'
+                >
+                    <span className='font-bold'> Evan Holster</span>
+                </Link>
+            </p>
+            <p>using Next.js, React, Node.js, TypeScript, Prisma, and Tailwind</p>
+            <p className='mt-2'>Deployed on
+                <Link
+                    href="https://www.vercel.com"
+                    target='_blank'
+                >
+                    <span className='font-bold'> Vercel</span>
+                </Link>
+            </p>
         </div>
     )
 }
