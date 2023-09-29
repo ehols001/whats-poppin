d3f1949e87a5c1a6b86066035a66864d4691c271
eholster <ehols001@odu.edu>
2023-09-29 12:28:32 -0400

    changed minor wording

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,8 @@ export const Footer = () => {
         <div className='flex items-center my-8'>
             <div className='flex flex-col text-center text-sm'>
                 <p>Designed and developed by <span className='font-bold'>Evan Holster</span></p>
-                <p>using Next.js, React, Node.js, TypeScript, Prisma, Tailwind</p>
+                <p>using Next.js, React, Node.js, TypeScript, Prisma, and Tailwind</p>
+                <p>Deployed with <span className='font-bold'>Vercel</span></p>
             </div>
         </div>
     )
