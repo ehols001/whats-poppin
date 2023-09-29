c22935e3bacfaf3c8b65bd2e26c0cc5fc193f500
eholster <ehols001@odu.edu>
2023-09-29 12:29:42 -0400

    removed unneccesary code

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,7 @@
 import ServicesHeader from './services-header';
 import { getServerSession } from 'next-auth/next';
-import { getService } from '@/prisma/db-scripts';
 
 export default async function Nav() {
     const session = await getServerSession();
-    const service = await getService({ serviceId: 'spotify' });
-
-    return <ServicesHeader user={session?.user} service={service} />;
+    return <ServicesHeader user={session?.user} />;
 }
