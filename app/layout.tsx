db1696e832926d2db3b87489245a44f9fc349b04
eholster <ehols001@odu.edu>
2023-11-15 10:48:33 -0500

    added new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Raleway } from 'next/font/google';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,13 +7,19 @@ export const metadata: Metadata = {
   description: 'Search for the most popular music, any genre, anywhere',
 }
 
+const raleway = Raleway({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={raleway.className}>
         <body>
             <div className='fixed w-full h-screen bg-gradient-to-br from-slate-400 to-emerald-400 -z-10'></div>
             <main>
