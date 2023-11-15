fb8a5d641c1197f9c4c73adf4dcb40906860764d
eholster <ehols001@odu.edu>
2023-11-15 10:50:20 -0500

    added dynamic tab title

--- app/services/[serviceId]/page.tsx
+++ app/services/[serviceId]/page.tsx
@@ -1,4 +1,17 @@
-export default function Service({ params }: { params: { serviceId: string }}) {
+//import { getTopArtistsByGenre } from "@/app/api/(services)/content/spotifyCalls";
+import { getAccessToken, getTopArtistsByGenre } from "@/app/api/(services)/content/spotify-api";
+
+export async function generateMetadata({ params }: { params: { serviceId: string }}) {
+    return {
+        title: `What's Poppin! | ${params.serviceId.replace(params.serviceId[0], params.serviceId[0].toUpperCase())}`
+    }
+}
+
+export default async function Service({ params }: { params: { serviceId: string }}) {
+
+    //const token = await getAccessToken();
+    const topArtists = await getTopArtistsByGenre({ genre: 'metalcore', limit: '1' });
+
     return (
         <div className="flex flex-col p-6">
             <p>{params.serviceId}</p>
